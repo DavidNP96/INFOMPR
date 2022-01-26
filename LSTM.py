897ea6048401a0c749a7b8865a5b9e71c9ba81ef
Robkarrs <72616493+Robkarrs@users.noreply.github.com>
2022-01-26 13:38:02 +0100

    new load_data

--- LSTM.py
+++ LSTM.py
@@ -1,94 +1,155 @@
 import numpy as np
 import pandas as pd
+import keras
+import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
+from keras.layers import GRU
+from keras.layers import Embedding
 from keras.layers import RNN
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
+import pickle
+import os
+from numpy.random import choice
+import string
 
-text = (open("data/wonderland.txt").read())
-text = text.lower()
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
 
-print("Downloaded Alice in Wonderland data with {} characters.".format(len(text)))
-print("FIRST 1000 CHARACTERS: ")
-print(text[:1000])
 
-characters = sorted(list(set(text)))
-words = sorted(list(set(text.split())))
+#amount of data to load
+INPUT_NUMBER = 1000
 
-text_as_words = text.split()
+#load data from file
+X = []
+full_text = '\n'
+
+fp = open("dataX.txt", "r")
+for i, line in enumerate(fp):
+    if i < INPUT_NUMBER:
+        line = line.lower()
+        #line = line.translate(str.maketrans("","", string.punctuation))
+        line = line.rstrip()
+        X.append(line)
+        full_text = full_text + ' '.join(line.split())
+    else:
+        break
+
+print(X[0])
+i= 0
+Y = []
+fp = open("dataY.txt", "r")
+for line in fp:
+    if i < INPUT_NUMBER:
+        if line.strip() != '':
+            Y.append(line.rstrip())
+        else:
+            Y.append('\n')    
+        i+=1
+        full_text = full_text + ' '.join(line.split())
+    else:
+        break
+
+words = X[0].split()
+words.append('\n')
+for i in range(1, len(X)):
+    sequence = X[i].split()
+    for j in range(0, len(sequence)):
+        words.append(sequence[j])
+
+
+for i in range(0, len(Y)):
+    words.append(Y[i])
+
+#unique words
+words_set = sorted(list(set(words)))
+
+#unique characters
+chars = sorted(list(set(full_text)))
+num_chars = len(chars)
 
 #create word mappings
-n_to_word = {n:word for n, word in enumerate(words)}
-word_to_n = {word:n for n, word in enumerate(words)}
+n_to_char = {n:char for n, char in enumerate(chars)}
+char_to_n = {char:n for n, char in enumerate(chars)}
 
-vocab_size = len(words)
-print('Number of unique words: ', vocab_size)
+#create Vectors for training
 
-X = []   # extracted sequences
-Y = []   # the target: follow up character for each sequence in X
 
-length = len(text_as_words)
-seq_length = 25
+# create vectors for the train text
+     
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(X)
+Y_train_counts = count_vect.transform(Y)
+# transform vectors using TF-IDF
 
-#Create X and Y vectors
+print("transform vectors using TF-IDF")
+tfidf_vectorizer = TfidfVectorizer().fit(X)
 
-for i in range(0, length - seq_length, 1):
-    sequence = text_as_words[i:i + seq_length]
-    label = text_as_words[i + seq_length]
-    X.append([word_to_n[word] for word in sequence])
-    Y.append(word_to_n[label])
+x_vec_train = tfidf_vectorizer.transform(X).toarray()
+y_vec_train = tfidf_vectorizer.transform(Y).toarray()
+x_vec_train = x_vec_train[:, :, None]
+y_vec_train = y_vec_train[:, :, None]
+
+X_train, X_test, y_train, y_test = train_test_split(x_vec_train, y_vec_train, test_size = 0.2, random_state = 1)
+
+# #create word mappings
+# n_to_word = {n:word for n, word in enumerate(words_set)}
+# word_to_n = {word:n for n, word in enumerate(words_set)}
+
+vocab_size = len(words_set)
+# print('Number of unique words: ', vocab_size)
+
+# X_train = []   # extracted sequences
+# Y_train = []   # the target: follow up character for each sequence in X
+
+# #Create X and Y vectors
+# for i in range(0, len(X)):
+#     X_train.append([word_to_n[word] for word in X[i].split()])
+#     Y_train.append(word_to_n[Y[i]])
     
-print('Number of extracted sequences:', len(X))
 
-X_modified = np.reshape(X, (len(X), seq_length, 1))
-X_modified = X_modified / float(len(characters))
-Y_modified = np_utils.to_categorical(Y)
+# X_modified = np.empty((len(X_train), 6))
 
-X_modified.shape, Y_modified.shape
+# for i in range(0, len(X_train)):
+#     while(len(X_train[i]) < 6):
+#         X_train[i].insert(0,0)
+#     while(len(X_train[i]) > 6):
+#         X_train[i].pop()
+#     X_modified[i] = np.array(X_train[i])
 
-print("X[0].shape = {}, Y[0].shape = {}".format(X_modified[0].shape, Y_modified[0].shape))
-print("X[0]: ", X_modified[0])
-print("Y[0]: ", Y_modified[0])
+# X_modified = X_modified / float(vocab_size)
+# #X_modified = np.reshape(X_modified, (len(X_modified), 6, 1))
 
+
+# Y_modified = np_utils.to_categorical(Y_train, num_classes=vocab_size)
+#print(y_train.shape)
+y_train = tf.squeeze(y_train, axis=-1)
 #Model using LSTM layers
 model = Sequential()
-model.add(LSTM(400, input_shape=(X_modified.shape[1], X_modified.shape[2]), return_sequences=True))
+# model.add(Embedding(vocab_size, X_modified.shape[1], input_length=X_modified.shape[1]))
+model.add(GRU(32, input_shape = X_train.shape[1:], return_sequences=True))
 model.add(Dropout(0.2))
-model.add(LSTM(400, return_sequences=True))
+model.add(GRU(32, return_sequences=True))
 model.add(Dropout(0.2))
-model.add(LSTM(400))
+model.add(GRU(32))
 model.add(Dropout(0.2))
-model.add(Dense(Y_modified.shape[1], activation='softmax'))
+model.add(Dense(y_train.shape[1], activation='softmax'))
 
-model.compile(loss='categorical_crossentropy', optimizer='adam')
+# model = Sequential()
+# model.add(LSTM(units=6, input_shape = X_train.shape[1:], return_sequences = True))
+# model.add(LSTM(units=6, return_sequences=True))
+# model.add(LSTM(units=6, return_sequences=True))
+# model.add(LSTM(units=1, return_sequences=True, name='output'))
+#model.add(Dense(vocab_size, activation='softmax'))
 
-model.fit(X_modified, Y_modified, epochs=10, batch_size=128)
+model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=25, batch_size=128)
+model.save('/content/drive/My Drive/Colab Notebooks/LSTM_V4')
 
 
-
-start = np.random.randint(0, len(X)-1) # or generate random   #random row from the X array
-string_mapped = list(X[start])
-full_string = [n_to_word[value] for value in string_mapped]
-
-# generating characters
-for i in range(400):
-    x = np.reshape(string_mapped,(1,len(string_mapped), 1))
-    x = x / float(len(characters))
-
-    pred_index = np.argmax(model.predict(x, verbose=0))
-    seq = [n_to_word[value] for value in string_mapped]
-    full_string.append(n_to_word[pred_index])
-
-    string_mapped.append(pred_index)
-    string_mapped = string_mapped[1:len(string_mapped)]
-
-# combining text
-txt=""
-for char in full_string:
-    txt = txt+char
-
-print(start)
-print(txt)
